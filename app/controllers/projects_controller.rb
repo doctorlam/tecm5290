7da1313249f3f895899bb180ec228e60e3a21556
Chris <chris.lam.unt@gmail.com>
2017-01-11 14:42:07 -0600

    reordering projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,7 +6,7 @@ class ProjectsController < ApplicationController
   # GET /projects
   # GET /projects.json
   def index
-    @projects = Project.all
+    @projects = Project.all.reorder(due: :asc)
 
   end
 
