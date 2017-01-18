3dc1aecee85db5f3e5d2d8007873ce1bc1b212db
Chris <chris.lam.unt@gmail.com>
2017-01-18 15:30:42 -0600

    fixed tutorial permissions

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -1,5 +1,7 @@
 class TutorialsController < ApplicationController
   before_action :set_tutorial, only: [:show, :edit, :update, :destroy]
+  before_filter :check_user, only: [:edit, :update, :destroy]
+  before_filter :authorize_admin, only: [:new, :create, :destroy, :edit]
 
   # GET /tutorials
   # GET /tutorials.json
@@ -70,5 +72,11 @@ class TutorialsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def tutorial_params
       params.require(:tutorial).permit(:name, :content, :video, {lesson_ids: []}, :lesson, :lesson_id)
+    end
+     def check_user
+      if current_user == authorize_admin
+        redirect_to root_url, alert: "You don't have permission to do that!"
+      end 
+
     end
 end
